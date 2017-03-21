6148bfd8421333d13a3cab77278c0dcfd99b2818
Ray Tsang <rayt@google.com>
2017-03-20 22:52:35 -0700

    Initial commit

---
+++ src/main/java/com/example/SpannerDemoApplication.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.spanner.core.SpannerTemplate;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class SpannerDemoApplication implements CommandLineRunner {
+  @Autowired
+  SpannerTemplate spannerTemplate;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpannerDemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... strings) throws Exception {
+     spannerTemplate.findAll(Trade.class)
+        .stream().forEach(System.out::println);
+
+    Trade t = Trade.builder().id(UUID.randomUUID().toString())
+        .symbol("AAPL")
+        .action("BUY")
+        .shares(5.0)
+        .price(100.0).build();
+
+    spannerTemplate.insert(t);
+  }
+}
